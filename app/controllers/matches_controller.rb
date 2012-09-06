bb3572ebcf738c78a3c808d985445ef020f96940
m-badov <mbadov@fiksu.com>
2012-09-06 19:12:14 -0400

    root page

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,4 +1,8 @@
 class MatchesController < ApplicationController
+  def index
+    render :text => "Welcome to HoN API"
+  end
+  
   def show
     match = Match.find_by_id(params[:id])
     render :json => match.to_json
